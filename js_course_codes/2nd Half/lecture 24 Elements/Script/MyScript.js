ca9bfe3304614c205b1e296602f26e55d4bdbf17
Kris Kuchinka <kriskuchinka@gmail.com>
2016-11-25 15:47:07 -0800

    Supposed to remove files

--- js_course_codes/2nd Half/lecture 24 Elements/Script/MyScript.js
+++
@@ -1,6 +0,0 @@
-var my=document.getElementById("menu");
-console.log("this is  element of type",menu.nodeType);
-console.log("the innner HTML is",menu.innerHTML);
-console.log("child Nodes ",menu.childNodes.length);
-var Link = document.getElementsByTagName("a");
-console.log("Links: ",Link.length);
